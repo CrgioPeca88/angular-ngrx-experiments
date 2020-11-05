e8e761360df984034315403c5d208c36cb4f0fa0
CrgioPeca88 <crgiopeca88@gmail.com>
2020-11-05 15:33:58 -0500

    Revert "[FEATURE] first commit with ngrx-exp project experimental about counter with ngrx"
    This reverts commit 1cfed6c801ca423a94df73854617067dc208fda3.

--- src/app/contador.actions.ts
+++
@@ -1,8 +0,0 @@
-import { createAction, props } from '@ngrx/store';
-
-export const inc = createAction('Incremento');
-export const dec = createAction('Decremento');
-export const rdm = createAction('Random');
-export const saltar = createAction('Saltar',
-  props<{num: number}>()
-);
