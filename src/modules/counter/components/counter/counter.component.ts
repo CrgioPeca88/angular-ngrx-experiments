72b96b2a1f77d31b05a6a36a72a7875a69947fd9
CrgioPeca88 <crgiopeca88@gmail.com>
2021-02-03 18:36:48 -0500

    [FEATURE] counter component adjusted with dynamic buttons - counter.component adjusted

--- src/modules/counter/components/counter/counter.component.ts
+++ src/modules/counter/components/counter/counter.component.ts
@@ -6,6 +6,12 @@ import { Observable } from 'rxjs';
 // Assets
 import * as action from '@counter/counter.actions';
 
+export interface Action {
+  buttonText: string;
+  buttonAction: (n?: number) => void;
+  applyButtonHover: boolean;
+}
+
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
@@ -13,26 +19,46 @@ import * as action from '@counter/counter.actions';
 })
 export class CounterComponent implements OnInit {
   public counter$: Observable<number>;
+  public actionsList: Action[];
 
-  constructor(private store: Store<{ counter: number }>) {}
+  constructor(private store: Store<{ counter: number }>) {
+    this.actionsList = [{
+      buttonText: "Sumar",
+      buttonAction: this.increment,
+      applyButtonHover: false
+    }, {
+      buttonText: "Restar",
+      buttonAction: this.decrement,
+      applyButtonHover: false
+    }, {
+      buttonText: "Random",
+      buttonAction: this.random,
+      applyButtonHover: false
+    }, {
+      buttonText: "Fijar",
+      buttonAction: this.saltar,
+      applyButtonHover: false
+    }]
+  }
 
   ngOnInit() {
     this.counter$ = this.store.pipe(select('counter'));
   }
 
-  public increment() {
+  private increment: () => void = () => {
     this.store.dispatch(action.inc());
   }
 
-  public decrement() {
+  private decrement: () => void = () => {
     this.store.dispatch(action.dec());
   }
 
-  public random() {
+  private random: () => void = () => {
     this.store.dispatch(action.rdm());
   }
 
-  public saltar(n: number) {
-    this.store.dispatch(action.saltar({num: n}));
+  private saltar: () => void = () => {
+    this.store.dispatch(action.saltar({num: 88}));
   }
+
 }
