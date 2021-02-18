68acae2171f74590f786bd0e433dea7e3c2415e6
CrgioPeca88 <crgiopeca88@gmail.com>
2021-02-18 17:26:19 -0500

    [FEATURE]  adding Renderer2 into rxjs-exam.component - rxjs-exam.component

--- src/modules/rxjs-examples/components/rxjs-exam/rxjs-exam.component.ts
+++ src/modules/rxjs-examples/components/rxjs-exam/rxjs-exam.component.ts
@@ -1,11 +1,37 @@
 // Dependencies
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, OnInit, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs-exam',
   templateUrl: './rxjs-exam.component.html',
   styleUrls: ['./rxjs-exam.component.less']
 })
-export class RxjsExComponent {
-  constructor() {}
+export class RxjsExComponent implements OnInit {
+
+  @ViewChild('oc') oc: ElementRef;
+  private observable$: Observable<string>;
+
+  constructor(
+    private renderer2: Renderer2
+  ) {
+    this.observable$ = Observable.create((o: any) => {
+      try {
+        o.next('Hello');
+        o.next('How are you?');
+        setInterval(() => {
+          o.next('I am good');
+        }, 2000);
+      } catch( error ) {
+        o.error(error);
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.observable$.subscribe((data: string) => {
+      console.log(data);
+    });
+  }
+
 }
