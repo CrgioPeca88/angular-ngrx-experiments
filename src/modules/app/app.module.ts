d6ed66f8c78232ebf335d1b76673a67f245fccc9
CrgioPeca88 <crgiopeca88@gmail.com>
2021-02-23 13:17:42 -0500

    [FEATURE] Store module moved from counter.module to app.module - counter.module file adjusted - app.module file adjusted

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,19 +1,22 @@
 // Dependencies
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
 
 // Assets
 import { AppRoutingModule } from '@app/app-routing.module';
 import { AppComponent } from '@app/components/app/app.component';
 import { CounterModule } from '@counter/counter.module';
 import { RxjsExamplesModule } from '@rxjs-exam/rxjs-examples.module';
+import { counterReducer } from '@counter/counter.reducers';
 
 @NgModule({
   imports: [
     BrowserModule,
     AppRoutingModule,
     CounterModule,
-    RxjsExamplesModule
+    RxjsExamplesModule,
+    StoreModule.forRoot({ counter: counterReducer})
   ],
   declarations: [
     AppComponent
--- src/modules/counter/counter.module.ts
+++ src/modules/counter/counter.module.ts
@@ -1,16 +1,13 @@
 // Dependencies
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { StoreModule } from '@ngrx/store';
 
 // Assets
 import { CounterComponent } from '@counter/components/counter/counter.component';
-import { counterReducer } from '@counter/counter.reducers';
 
 @NgModule({
   imports: [
-    BrowserModule,
-    StoreModule.forRoot({ counter: counterReducer})
+    BrowserModule
   ],
   declarations: [
     CounterComponent
