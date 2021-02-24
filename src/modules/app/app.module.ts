5f1a12e42f96eed708bb0fea1f95a2a18a460d31
CrgioPeca88 <crgiopeca88@gmail.com>
2021-02-24 17:22:46 -0500

    [FEATURE] reducers created and the ngrx modules added into app.module - app.module file adjusted - chat.reducers file adjusted

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,22 +1,36 @@
 // Dependencies
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { StoreModule } from '@ngrx/store';
+import { StoreModule, ActionReducerMap } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreRouterConnectingModule, routerReducer } from '@ngrx/router-store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 // Assets
+import { environment } from '@environments/environment';
 import { AppRoutingModule } from '@app/app-routing.module';
 import { AppComponent } from '@app/components/app/app.component';
 import { HomeComponent } from './components/home/home.component';
 import { counterReducer } from '@counter/counter.reducers';
+import { reducers, metaReducers, State } from '@chat/chat.reducers';
+
+const counterR = { counter: counterReducer };
+const resultReducers: ActionReducerMap<State|any> = { ...counterR, ...reducers };
+const NGRX_IMPORTS = [
+  StoreModule.forRoot(resultReducers, { metaReducers }),
+  StoreRouterConnectingModule.forRoot({ stateKey: 'router' }),
+  EffectsModule.forRoot([]),
+  StoreDevtoolsModule.instrument({
+    name: 'AngularChatNgRx',
+    logOnly: environment.production
+  })
+];
 
 @NgModule({
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({ counter: counterReducer})
+    ...NGRX_IMPORTS
   ],
   declarations: [
     AppComponent,
--- src/modules/chat/chat.reducers.ts
+++ src/modules/chat/chat.reducers.ts
@@ -7,5 +7,36 @@ import {
   MetaReducer
 } from '@ngrx/store';
 import { environment } from '@environments/environment';
-//import { RouterStateUrl } from '@core/utils';
 import * as fromRouter from '@ngrx/router-store';
+import { storeFreeze } from 'ngrx-store-freeze';
+
+//Assets
+import { RouterStateUrl } from '@core/utils';
+import * fromAuth from '@chat/components/auth/reducers/auth.reducer'
+
+export interface State {
+  auth: fromAuth.state,
+  router: fromRouter.RouterReducerState<RouterStateUrl>;
+}
+
+export const reducers: ActionReducerMap<State|any> = {
+  auth: fromAuth.AuthReducer,
+  router: fromRouter.routerReducer
+}
+
+export function logger(reducer: ActionReducer<State>): ActionReducer<State> {
+  return function(state: State, action: any): State {
+    console.log(`%c State ==>`, `color: white; background-color: #a829c3; border-radius: 4px`, state);
+    console.log(`%c Action ==>`, `color: white; background-color: #a829c3; border-radius: 4px`, action);
+    return reducer(state, action);
+  }
+}
+
+export const metaReducers: MetaReducer<State>[] = !environment.production ? [logger, storeFreeze] : [];
+
+export const getAuthState = createFeatureSelector<fromAuth.State>('auth');
+
+export const getAuth = createSelector(
+  getAuthState,
+  fromAuth.getAuthState
+)
