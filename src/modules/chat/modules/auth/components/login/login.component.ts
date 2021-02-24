4caa0f16dd956e947df249de6e9ebb52a59f3a72
CrgioPeca88 <crgiopeca88@gmail.com>
2021-02-24 18:41:13 -0500

    [FEATURE] login inputs added and login component functions added - login.component adjusted - auth.service created

--- src/modules/chat/modules/auth/components/login/login.component.ts
+++ src/modules/chat/modules/auth/components/login/login.component.ts
@@ -1,11 +1,25 @@
 // Dependencies
 import { Component } from '@angular/core';
 
+// Assets
+import { IUser } from '@core/interfaces/IUser.model';
+import { AuthService } from '@chat/modules/auth/services/auth.service';
+
 @Component({
   selector: 'chat-auth-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.less']
 })
 export class LoginComponent {
-  constructor() {}
+
+  public user: IUser;
+
+  constructor(private authService: AuthService) {  }
+
+  public login(): void {
+    this.authService.login(this.user).subscribe((res: any) => {
+      console.log("--->", res);
+    });
+  }
+
 }
--- src/modules/chat/modules/auth/services/auth.service.ts
+++ src/modules/chat/modules/auth/services/auth.service.ts
@@ -1,2 +1,19 @@
 // Dependencies
 import { Injectable } from '@angular/core';
+import { from, Observable } from 'rxjs';
+
+// Assets
+import { IUser } from '@core/interfaces/IUser.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+
+  public login(user: IUser): Observable<any> {
+    return from([true]);
+  }
+
+}
