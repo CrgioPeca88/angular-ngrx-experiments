a5bb49c911a9708928bc8e89b0040fd086504e52
CrgioPeca88 <crgiopeca88@gmail.com>
2021-03-05 17:31:27 -0500

    [FEATURE] NGRX logger added - app.module adjusted

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -2,9 +2,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { StoreModule, ActionReducerMap } from '@ngrx/store';
+import { StoreModule, ActionReducerMap, ActionReducer, MetaReducer } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { storeFreeze } from 'ngrx-store-freeze';
 
 // Assets
 import { environment } from '@environments/environment';
@@ -14,8 +15,19 @@ import { HomeComponent } from './components/home/home.component';
 import { counterReducer } from '@counter/counter.reducers';
 
 const counterR: ActionReducerMap<any> = { counter: counterReducer };
+
+export function logger(reducer: ActionReducer<any>): ActionReducer<any> {
+  return function(state: any, action: any): any {
+    console.log(`%c State ==>`, `color: white; background-color: #a829c3; border-radius: 4px`, state);
+    console.log(`%c Action ==>`, `color: white; background-color: #a829c3; border-radius: 4px`, action);
+    return reducer(state, action);
+  }
+}
+
+export const metaReducers: MetaReducer<any>[] = !environment.production ? [logger, storeFreeze] : [];
+
 const NGRX_IMPORTS = [
-  StoreModule.forRoot(counterR),
+  StoreModule.forRoot(counterR, { metaReducers }),
   EffectsModule.forRoot([]),
   StoreDevtoolsModule.instrument({
     name: 'AngularChatNgRx',
