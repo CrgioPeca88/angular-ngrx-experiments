7f337205f52fd28d44c7ace1600fbf8907555ae2
CrgioPeca88 <crgiopeca88@gmail.com>
2021-03-08 18:39:55 -0500

    [FEATURE] ngrx/store-devtools added - ngrx-config adjusted

--- src/modules/core/constants/ngrx-config.constants.ts
+++ src/modules/core/constants/ngrx-config.constants.ts
@@ -25,7 +25,6 @@ export const NGRX_CONFIG = [
   EffectsModule.forRoot([]),
   StoreDevtoolsModule.instrument({
     name: 'AngularChatNgRx',
-    logOnly: environment.production,
     maxAge: 25
   })
 ];
